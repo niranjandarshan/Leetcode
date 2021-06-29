7be8c3ea60b5512c278b8bf5cbcfc27ea149e849
Niranjan <51872323+niranjandarshan@users.noreply.github.com>
2021-06-29 22:03:08 +0530

    Create Add Two Number(linkedlist).cpp

---
+++ Add Two Number(linkedlist).cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+   ListNode * res = new ListNode(-1); .
+ListNode * curr=res; 
+int carry=0,n1,n2,sum;
+while(l1 || l2)
+{
+  if(l1){
+    n1=l1->val;
+  }
+  else{
+  n1=0;
+  }
+if(n2){
+n2=l2->val;
+}
+  else{
+  n2=0;
+  }
+
+ sum=n1 + n2 + carry;
+carry=sum/10;
+  sum=sum%10;
+curr->next = new ListNode(sum);
+curr=curr->next;
+
+     if(l1)
+         l1=l1->next;
+     
+     if(l2)
+         l2=l2->next;
+ }
+ if(carry)
+     curr->next= new ListNode(1);   //if carry>0 then put carry at new created last node.
+ 
+ return res->next;
+    }
+};
